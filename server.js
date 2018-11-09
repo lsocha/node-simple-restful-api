a818dbdc8ffc49d75b7b3f90c33876edcad30e1c
Łukasz Socha <lukskywaker@gmail.com>
2018-11-09 16:19:29 +0100

    code formatting

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb');
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
@@ -23,6 +23,6 @@ app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
 
-app.use(function(req, res) {
+app.use(function (req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 });
